cb1e195aa955cfd262a5a391ef966c2abcbc9f89
olivier.bonheur <olivier.bonheur@gmail.com>
2020-09-27 22:28:53 +0200

    Working on Controller

--- TourGuide/src/main/java/gpsModule/controller/GpsController.java
+++ TourGuide/src/main/java/gpsModule/controller/GpsController.java
@@ -3,6 +3,8 @@ package gpsModule.controller;
 import gpsModule.service.IGpsService;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,19 +15,24 @@ import java.util.UUID;
 
 @RestController
 public class GpsController {
+    private Logger logger = LoggerFactory.getLogger(GpsController.class);
 
     @Autowired
     IGpsService gpsService;
 
     @RequestMapping("/getUserLocation")
-    public VisitedLocation getUserLocation(@RequestParam UUID userId){
-        VisitedLocation visitedLocation = gpsService.getUserLocation(userId);
+    public VisitedLocation getUserLocation(@RequestParam String userId){
+        logger.debug("Request getUserLocation");
+        VisitedLocation visitedLocation = gpsService.getUserLocation(UUID.fromString(userId));
+        logger.debug("Response : UUID=" + visitedLocation.userId +" Lat=" + visitedLocation.location.latitude + " Lon=" + visitedLocation.location.longitude + " Date=" + visitedLocation.timeVisited +"/n");
         return visitedLocation;
     }
 
     @RequestMapping("/getAttractions")
     public List<Attraction> getAttractions() {
+        logger.debug("Request getAttractions");
         List<Attraction> allAttractions = gpsService.getAttractions();
+        logger.debug("Response : Nb Attractions=" + allAttractions.size());
         return allAttractions;
     }
 }
