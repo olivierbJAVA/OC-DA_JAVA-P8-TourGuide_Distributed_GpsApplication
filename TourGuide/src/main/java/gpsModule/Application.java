e17e2ecd85934b6fd022eddac77cfa4e757941bd
olivier.bonheur <olivier.bonheur@gmail.com>
2020-10-06 01:06:22 +0200

    Work on Application

--- TourGuide/src/main/java/gpsModule/Application.java
+++ TourGuide/src/main/java/gpsModule/Application.java
@@ -2,17 +2,24 @@ package gpsModule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Locale;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         //Added to fix NumberFormatException due to decimal number separator
         Locale.setDefault(new Locale("en", "US"));
 
-        SpringApplication.run(gpsModule.Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
